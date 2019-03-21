58653e63b981f4be57f00cfd19630a9b6c3db9f1
Daniel Foose <dpfoose@gmail.com>
2019-03-21 15:25:06 -0400

    fixed __init__.py

--- omics_dashboard_client/hdf_tools/__init__.py
+++ omics_dashboard_client/hdf_tools/__init__.py
@@ -1,5 +1,5 @@
-from collection_tools import get_dataframe, update_array, validate_update, get_dataset
-from h5_merge import h5_merge
-from metadata_tools import get_file_attributes, get_file_attribute_dtypes, get_collection_metadata, get_collection_info, \
+from omics_dashboard_client.hdf_tools.collection_tools import get_dataframe, update_array, validate_update, get_dataset
+from omics_dashboard_client.hdf_tools.h5_merge import h5_merge
+from omics_dashboard_client.hdf_tools.metadata_tools import get_file_attributes, get_file_attribute_dtypes, get_collection_metadata, get_collection_info, \
     get_dataset_paths, get_csv, get_group_info, get_dataset_info, get_all_dataset_info, get_datasets, update_metadata, \
     create_empty_file, approximate_dims, add_column
